20c0a6503a275a1094391ff8c1857914032e5994
NoamN9 <44999487+NoamN9@users.noreply.github.com>
2020-12-02 16:51:55 +0200

    add db

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const bcrypt = require('bcrypt-nodejs');
-const cors = require('cors');
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+var knex = require('knex')
 
-const database = {
+const db =knex({
+  client: 'pg',
+  connection: {
+    host : 'localhost',
+    user : 'postgres',
+    password : '',
+    database : 'postgres'
+  }
+});
+
+
+
+
+
+const removesoon = {
   users: [
     {
       id: "123",
@@ -21,35 +36,34 @@ const database = {
   ],
 };
 
-
-
-
 app.use(bodyParser.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-    res.send(database);  
-  });
-
+  //also didnt deside what to do with it
+  res.send(db);
+});
 
 app.post("/login", (req, res) => {
+  //check if the user in the db
   const user = {
-      name : req.body.user,
-      password: req.body.password
-  }
-  console.log(user)
-res.json("succsess");
-
+    name: req.body.user,
+    password: req.body.password,
+  };
+  console.log(user);
+  res.json("succsess");
 });
 
 app.post("/register", (req, res) => {
+  //add user to the db
   res.send("add user to database");
 });
 
 app.get("/profile/:userid", (req, res) => {
-    res.send("bring back user with the mathing id");
-  });
-
+  //no sure about this shit
+  res.send("bring back user with the mathing id");
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+
