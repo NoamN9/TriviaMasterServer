70ef848a97bbf71f398ee289cc769625877b8733
NoamN9 <44999487+NoamN9@users.noreply.github.com>
2020-12-08 11:41:42 +0200

    setting up auth

--- index.js
+++ index.js
@@ -3,66 +3,83 @@ const app = express();
 const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt-nodejs");
 const cors = require("cors");
-var knex = require('knex')
+var knex = require('knex');
+const { response } = require("express");
 
 const db =knex({
   client: 'pg',
   connection: {
     host : 'localhost',
     user : 'postgres',
-    password : '',
+    password : 'd33d41',
     database : 'postgres'
   }
 });
 
 
 
-
-
-const removesoon = {
-  users: [
-    {
-      id: "123",
-      name: "john",
-      email: "john@gmail.com",
-      password: "123456",
-    },
-    {
-      id: "1234",
-      name: "Noam",
-      email: "noamn99@gmail.com",
-      password: "bestpasswordever",
-    },
-  ],
-};
-
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  //also didnt deside what to do with it
-  res.send(db);
-});
+
 
 app.post("/login", (req, res) => {
-  //check if the user in the db
-  const user = {
-    name: req.body.user,
-    password: req.body.password,
-  };
-  console.log(user);
-  res.json("succsess");
+  const {email,password} = req.body
+  db.select('email','hash').from('login')
+  .where('email','=',email)
+  .then(data=>{
+    const isValid = bcrypt.compareSync(password,data[0].hash);
+    if(isValid){
+      return db.select('*').from('users')
+      .where('email','=',email)
+      .then(user=> res.json(user[0]))
+      .catch(err=> res.status(400).json('unable to get user'))
+    }
+    else{
+      res.status(400).json('wrong credentials')
+    }
+  })
+  .catch(err=>res.status(400).json('wrond credentials'))
+  
 });
 
 app.post("/register", (req, res) => {
-  //add user to the db
-  res.send("add user to database");
+  const {email,user,password} = req.body
+  const hash = bcrypt.hashSync(password)
+  db.transaction(trx => {
+    trx.insert({
+      hash:hash,
+      email:email
+    })
+    .into('login')
+    .returning('email')
+    .then(loginEmail=> {
+      return trx('users')
+      .returning('*')
+      .insert({
+        email: loginEmail[0],
+        username:user
+    })
+    .then((user)=>res.json(user[0]))
+  })
+  .then(trx.commit)
+  .catch(trx.rollback)
+  })
+  .catch(err => res.status(400).json('unable to register'))
 });
 
-app.get("/profile/:userid", (req, res) => {
-  //no sure about this shit
-  res.send("bring back user with the mathing id");
-});
+app.put('/points',(req,res)=>{
+  const {id,addedpoint} = req.body;
+ 
+  db('users')
+  .where('id','=',id)
+  .increment('points',addedpoint)
+  .returning('points')
+  .then(points => {
+    res.json(points[0]);
+  })
+  .catch(err=>res.status(400).json('unable to get points')) 
+})// if i enter wrong id it will return nothing and not a error need to think about it.
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
